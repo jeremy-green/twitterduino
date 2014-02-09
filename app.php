a0621ae52ea08fe1717123095faf631afbbe7ce2
Jeremy Green <mutatedcreativity@gmail.com>
2014-02-09 10:39:18 -0500

    adding the files

---
+++ app.php
@@ -0,0 +1,65 @@
+<?php
+global $serial;
+require 'php-serial/php_serial.class.php';
+$serial = new \phpSerial\phpSerial;
+$serial->phpSerial();
+$serial->deviceSet('/dev/ttyACM0');
+$serial->confBaudRate(9600);
+$serial->confParity("none");
+$serial->confCharacterLength(8);
+$serial->confStopBits(1);
+$serial->deviceOpen();
+
+require 'twitter-api-php/TwitterAPIExchange.php';
+$oldTweet = '';
+
+//wait for board to boot
+sleep(5);
+
+handleIt($oldTweet);
+
+function handleIt($oldTweet) {
+  $newTweet = getLatestTweet();
+  print_r($newTweet . PHP_EOL);
+  if ($newTweet != $oldTweet) {
+    sendLatestTweet($newTweet);
+  }
+  $oldTweet = $newTweet;
+  sleep(5);
+  handleIt($oldTweet);
+}
+
+function sendLatestTweet($text) {
+  global $serial;
+  $message = explode("\n", wordwrap($text, 16));
+  $len = count($message);
+
+  for ($i=0; $i < $len; $i+=2) {
+    $output = trim($message[$i]);
+    if ($i+1 != $len) {
+      $output .= ' ' . trim($message[$i+1]);
+    }
+    $serial->sendMessage($output);
+    //some reading time
+    sleep(4);
+  }
+}
+
+function getLatestTweet()
+{
+  $url = 'https://api.twitter.com/1.1/search/tweets.json';
+  $getfield = '?q=%23walkingdead&count=1';
+  $requestMethod = 'GET';
+  $settings = array(
+    'oauth_access_token' => 'YOUR_OAUTH_ACCESS_TOKEN',
+    'oauth_access_token_secret' => 'YOUR_OAUTH_ACCESS_TOKEN_SECRET',
+    'consumer_key' => 'YOUR_CONSUMER_KEY',
+    'consumer_secret' => 'YOUR_CONSUMER_SECRET'
+  );
+  $twitter = new TwitterAPIExchange($settings);
+  $twitter_json = $twitter->setGetfield($getfield)
+                 ->buildOauth($url, $requestMethod)
+                 ->performRequest();
+  $twitter_json = json_decode($twitter_json);
+  return $twitter_json->statuses[0]->text;
+}
---
+++ twitterduino.ino
@@ -0,0 +1,35 @@
+//twitterduino.ino
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int contrastPin = 10;
+const int contrast = 20;
+
+char character;
+
+void setup() {
+  lcd.begin(16, 2);
+  analogWrite(contrastPin, contrast);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+
+    delay(100);
+    int i = 0;
+
+    while (Serial.available() > 0) {
+      character = Serial.read();
+      //Serial.println(character);
+      i++;
+      lcd.write(character);
+      if (i >= 16) {
+        lcd.setCursor(i - 16, 1);
+      }
+    }
+  }
+}
